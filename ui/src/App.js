943ff33f6c8cdc23a69f79d8d6f7549cdcab6c3e
act18 <act18@sfu.ca>
2022-10-15 13:12:52 -0700

    structure of html

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,14 +7,23 @@ function App() {
     <form method="POST">
       <label>Caption</label>
       <input type="text" id="caption"></input>
+      <br></br>
       <label>Prompt</label>
       <input type="text" id="prompt"></input>
+      <br></br>
       <label>Diary Entry</label>
-      <input type="text" id="entry"></input>
-      <button>Submit</button>
+      <textarea type="text" id="entry"></textarea>
+      <br></br>
+      <button type="submit">Submit</button>
     </form>
 
   );
 }
+
+const styles = StyleSheet.create({
+
+
+
+})
 
 export default App;
