c2b6855ee32e1a9b4489f9a79ea3482c31c8ad99
act18 <act18@sfu.ca>
2022-10-15 12:57:46 -0700

    inital form

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <form method="POST">
+      <label>Caption</label>
+      <input type="text" id="caption"></input>
+      <label>Prompt</label>
+      <input type="text" id="prompt"></input>
+      <label>Diary Entry</label>
+      <input type="text" id="entry"></input>
+      <button>Submit</button>
+    </form>
+
   );
 }
 
