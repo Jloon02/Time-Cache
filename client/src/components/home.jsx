b08b6c1ced9fc89f306b5475f95bc4eb55686aed
Andrew Lee <andrewlee011@hotmail.com>
2022-10-15 17:28:43 -0700

    home.jsx changes

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,11 +1,8 @@
 // Import FirebaseAuth and firebase.
-import React from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import {React, useEffect, useState} from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,55 +17,30 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+var app = firebase.initializeApp(firebaseConfig);
 
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-};
+const auth = getAuth(app);
 
-function Login() {
+export default function Home() {
+  const [caption, setCaption] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [date, setDate] = useState("");
+  const [uid, setUid] = userState("test")
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      console.log("User signed in")
+      setUid(user.uid)
+    } else {
+      console.log("User not signed in")
+    }
+  });
+
   return (
-<body>
-      <head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"></link>
-      <link rel="stylesheet" href="./loginPage.css"></link>
-      <script src="./loginPage.js" defer></script>
-      </head>
-      <div id="welcome_container">
-        <h1 class="welcome_message">Please Log-In: </h1>
-      </div>
-
-      <div class="center">
-        <div id="login_container">
-          <form>
-            <label for="username"></label>
-            <input type="text" id="username" placeholder="Username" name="username" required></input>
-            <label for="password"></label>
-            <input type="password" id="password" placeholder="Password" name="password" required></input>
-            <div id="incorrect_credentials" class="center">
-              <b>Your username or password is not correct!</b>
-            </div>
-
-            <div class="buttons">
-              <button type="button" id="login" onclick="handleLogin()">Log-In</button>
-            </div>
-          </form>
-
-        </div>
-      </div>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-    </body>
-
-  );
+    <div>
+      {uid}
+    </div>
+  )
 }
-
-export default Login
